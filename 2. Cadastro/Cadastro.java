fe7edb9f5e3dfe812a9374854aa5df18c1a6b801
tiagolimar <tiagolima.r2@gmail.com>
2023-08-01 22:19:43 -0300

    trabalhando no cadastro

--- 2. Cadastro/Cadastro.java
+++ 2. Cadastro/Cadastro.java
@@ -7,6 +7,24 @@ public class Cadastro{
         Scanner scanner = new Scanner(System.in);
         List<Peoples> peoples = new ArrayList<Peoples> ();
 
+        for (int i=0;i<5;i++){
+            System.out.println(String.format("Pessoa %d",i));
+
+            System.out.println(String.format("Digite o nome: "));
+            String nome = scanner.nextLine();
+
+            System.out.println("Digite a idade: ");
+            int idade = Integer.parseInt(scanner.nextLine());
+
+            System.out.println("Digite o peso: ");
+            double peso = Double.parseDouble(scanner.nextLine());
+
+            System.out.println("Digite a altura: ");
+            double altura = Double.parseDouble(scanner.nextLine());
+
+            Pessoa pessoa = new Pessoa(nome, idade, peso, altura);
+            pessoas.add(pessoa);
+        }
 
     }
 }
